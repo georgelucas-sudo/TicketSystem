7973681b0c20ad08bd382e5f753bab3204b5e438
Yiga George Lucas <georgedalucas111@gmail.com>
2023-03-05 04:34:51 +0300

    initial commit

---
+++ ticket.py
@@ -0,0 +1,42 @@
+#a program prints out tickets for a theater
+
+#input
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+trip = input("Enter the name of the trip: ")
+time = input("Enter the time of the Trip: ")
+price = float(input("Enter the price of the ticket: "))
+#process
+total = price * 10
+#output
+print("Name: ", name) 
+print("Age: ", age)
+print("Trip Name: ", trip)
+print("Time: ", time)
+print("Price: ", price)
+print("Total: ", total)
+
+# Path: ticket.py
+# Compare this snippet from octo.py:
+# #Convert decimal numbers to octal  numbers
+#a fuel pump program
+#input
+price = float(input("Enter the price of the gas: "))
+#process
+total = price * 1.13
+#output
+print("Price: ", price)
+print("Total: ", total)
+
+# Path: octo.py
+#unit conversion program
+#input
+miles = float(input("Enter the number of miles: "))
+#process
+kilometers = miles * 1.609
+#output
+print("Miles: ", miles)
+print("Kilometers: ", kilometers)
+
+# Path: unit.py
+
